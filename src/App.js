775c5cde2a431c9b4b87a086d07bf738ded3e72e
Kyle Knaggs <kyle@Kyles-MacBook-Pro.local>
2020-09-20 12:06:59 -0400

    Use useEffect to fetch data and update a component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  // Declare a new state variable, which we'll call "count"
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
+function App () {
+  const [comments, setComments] = useState(null);
+  useEffect(function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      // Limit the comments to the first 5 responses
+      .then(json => json.slice(0, 5))
+      // Wait 1 second before displaying the data
+      .then((json) => {
+        setTimeout(() => {
+          setComments(json);
+        }, 1000)
+      })
   });
 
+  if (!comments) {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
   return (
-    <div className="App">
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
+    <ul>
+      {comments.map(function (comment, index) {
+        const {id, title, body} = comment;
+
+        return (
+          <li key = {index}>
+            <p>{`${id}: ${title}`}</p>
+            <p>{body}</p>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
