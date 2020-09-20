838412dbd68e230923ec3c7336fdbf36572d9895
Kyle Knaggs <kyle@Kyles-MacBook-Pro.local>
2020-09-19 21:35:01 -0400

    Add effect without cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
   return (
     <div className="App">
